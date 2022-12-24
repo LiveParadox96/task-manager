40dcf0c984383e86f29daaeb4f9900900f8b701d
Maksim El'kin <LiveParadox@vk.com>
2022-12-24 19:54:53 +0300

    add parameters screen

---
+++ screens/TasksScreen.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+import { View } from "../components/Themed";
+import TaskList from "../components/TaskList";
+import React from "react";
+
+export default function TasksScreen() {
+  return (
+    <View style={styles.container}>
+      <TaskList />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});

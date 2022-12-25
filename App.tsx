c8639259c0a3a5d569fbe185ecddfa18da182e4b
Maksim El'kin <LiveParadox@vk.com>
2022-12-25 13:58:46 +0300

    add changes to routing

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NativeBaseProvider } from "native-base";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import Navigation from "./navigation";
+import { LogBox } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+LogBox.ignoreLogs(["Warning: ..."]);
+LogBox.ignoreAllLogs();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NativeBaseProvider>
+        <Provider store={store}>
+          <SafeAreaProvider>
+            <Navigation />
+            <StatusBar />
+          </SafeAreaProvider>
+        </Provider>
+      </NativeBaseProvider>
     );
   }
 }
